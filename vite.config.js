d7498875bfe6bf6c24cb81fcf64182832241e701
Mandeep Nagar <165379124+mandeep140@users.noreply.github.com>
2025-04-04 23:01:55 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+    server: {
+     host: '0.0.0.0', // Bind to all interfaces
+     port: 3000,
+     allowedHosts: ['the-production.onrender.com'],
+   },
 })
